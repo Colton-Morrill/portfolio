637c7c450c8d2ca0413ecbfe432987aace60ce34
coltonmorrill <colton@tacksystems.com>
2022-10-28 01:33:44 -0600

    Nextjs Link fix.

--- components/navbar.js
+++ components/navbar.js
@@ -1,6 +1,7 @@
 import {React, Fragment} from 'react'
 import { Menu, Transition, Disclosure } from '@headlessui/react';
 import { Bars3Icon, BellIcon, XMarkIcon } from '@heroicons/react/24/outline';
+import Link from 'next/link'
 
 const navigation = [
     { name: 'Projects', href: '#projects', current: false },
@@ -32,10 +33,10 @@ const Navbar = () => {
                 </Disclosure.Button>
               </div>
               <div className="flex flex-1 items-center justify-center sm:items-stretch sm:justify-start">
-                <a href="/" className="flex flex-shrink-0 items-center">
+                <Link href="/" className="flex flex-shrink-0 items-center">
                 <img className='block h-8 w-auto lg:hidden' src='/cm-logo-01.svg' alt="Promotional Image of the Little Patriots mobile application" />
                 <img className='hidden h-8 w-auto lg:block' src='/cm-logo-01.svg' alt="Promotional Image of the Little Patriots mobile application" />
-                </a>
+                </Link>
                 <div className="hidden sm:block ml-auto">
                   <div className="flex space-x-4">
                     {navigation.map((item) => (
