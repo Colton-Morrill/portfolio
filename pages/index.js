46f66cedfdec3e823c4bd91f2767e19888ad2cf8
coltonmorrill <colton@tacksystems.com>
2022-11-30 18:16:47 -0700

    Updates h1

--- pages/index.js
+++ pages/index.js
@@ -85,13 +85,9 @@ function calcDate(date1,date2) {
       <Navbar />
       <div id='projects2' className='scroll-spacer'></div>
       <main className="flex justify-center items-center flex-col h-screen w-full relative mt-40 lg:-mt-16 mb-52 lg:mb-0">
-      
-        <h1 className='text-9xl font-bold lg:-mr-14' data-scroll data-scroll-direction='horizontal' data-scroll-speed='2'>
-          I<span className="text-transparent bg-clip-text bg-gradient-to-r from-green-400 to-cyan-600 font-black text-left"> $H!T</span> Done
-        </h1>
-        {/* <h1 className='text-8xl font-bold lg:-mr-8' data-scroll data-scroll-direction='horizontal' data-scroll-speed='2'>
+        <h1 className='text-8xl font-bold lg:-mr-8' data-scroll data-scroll-direction='horizontal' data-scroll-speed='2'>
           I <span className="text-transparent bg-clip-text bg-gradient-to-r from-purple-400 to-blue-600 font-black text-left">Design</span>, I<span className="text-transparent bg-clip-text bg-gradient-to-r from-green-400 to-cyan-600 font-black text-left"> Code</span>, I Get It <span className="text-transparent bg-clip-text bg-gradient-to-r from-yellow-400 to-orange-600 font-black text-left">Done</span>
-        </h1> */}
+        </h1>
         <div className='mt-20' data-scroll data-scroll-direction='horizontal' data-scroll-speed='-2'>
           <p className='font-semibold text-left lg:text-center text-2xl'>Hello, I am Colton Morrill, an experienced <span className='linear-wipe font-bold'>Frontend Developer</span> and <span className='linear-wipe font-bold'>Graphic Designer</span>.</p>
           <p className='font-semibold text-left lg:text-center text-2xl mt-16'>I specialize in React, Next.js, Flutter, and the .NET framework</p>
