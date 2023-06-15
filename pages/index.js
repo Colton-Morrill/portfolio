94dd4fb469506d3b02574b57631af0d3bb682cdb
coltonmorrill <colton.morrill@vibilitysystems.com>
2023-06-14 21:12:33 -0600

    Project updates. Bio updates.

--- pages/index.js
+++ pages/index.js
@@ -100,8 +100,8 @@ export default function Home() {
       </div>
 
       <div className='flex justify-end mb-16 lg:mb-40 flex-col lg:flex-row'>
-        <ProjectCard UrlPath="https://tacksystems.com/" ImagePath="/tack-updated.mp4" ProjectTitle="Tack Systems LLC" ProjectSoftware=".NET" Scroll="1" />
-        <ProjectCard UrlPath="https://play.google.com/store/apps/details?id=com.tacksystems.littlepatriots&hl=en_US&gl=US" ImagePath="/loginmockup-01.svg" ImageAlt="Promotional Image of the Little Patriots mobile application" ProjectTitle="Little Patriots" ProjectSoftware="Google Flutter" Spacer="ml-0 lg:ml-8 mt-16 lg:mt-0" />
+        <ProjectCard UrlPath="https://portal.aptogenix.com/Account/Login?ReturnUrl=%2F" ImagePath="/apto.mp4" ProjectTitle="AptoGenix" ProjectSoftware=".NET" Scroll="1" />
+        <ProjectCard UrlPath="#" ImagePath="/mwc.png" ImageAlt="Promotional Image of the My Wellness Curriculum mobile application" ProjectTitle="My Wellness Curriculum" ProjectSoftware="React Native" Spacer="ml-0 lg:ml-8 mt-16 lg:mt-0" />
       </div>
 
       <div className='flex mb-36 lg:mb-40 flex-col lg:flex-row'>
@@ -112,9 +112,9 @@ export default function Home() {
       <div className='mt-8 mb-40 flex flex-col items-center'>
         <h3 className='text-4xl font-bold text-center mb-16'>More Projects</h3>
         <div className='flex flex-col flex-wrap lg:flex-row justify-start lg:justify-center items-start lg:items-center ml-0 lg:ml-36'>
+          <ProjectCardSmall ProjectUrl="https://my.wellnesscurriculum.com/mwc/login" ImagePath="/icon.png" ImageAlt="Icon of the Vibility Logo" ProjectName="Vibility Member Portal" ProjectSoftware="Next.js" />
           <ProjectCardSmall ImagePath="/tackicon.png" ImageAlt="Icon of the Tack Systems Logo" ProjectName="Tackify" ProjectSoftware=".NET" />
-          <ProjectCardSmall ImagePath="/tackicon.png" ImageAlt="Icon of the Tack Systems Logo" ProjectName="TackUI" ProjectSoftware=".NET/SCSS" />
-          <ProjectCardSmall ImagePath="/advisor-white.png" ImageAlt="Icon of the Vibility Logo" ProjectName="Vibility Advisor" ProjectSoftware=".NET/SCSS" />
+          <ProjectCardSmall ProjectUrl="https://play.google.com/store/apps/details?id=com.tacksystems.littlepatriots&hl=en_US&gl=US" ImagePath="/little-pat.png" ImageAlt="Icon of the Tack Systems Logo" ProjectName="Little Patriots" ProjectSoftware="Google Flutter" />
           <ProjectCardSmall ProjectUrl="https://play.google.com/store/apps/details?id=com.ctay.robal&hl=en_US&gl=US" ImagePath="/robal.png" ImageAlt="Icon of the Robal Logo" ProjectName="Robal" ProjectSoftware=".NET" />
           <ProjectCardSmall ImagePath="/docs-logo.png" ImageAlt="Icon of the DOCS Logo" ProjectName="Document Imaging System" ProjectSoftware="XAML" />
           <ProjectCardSmall ProjectUrl="https://vaporwave.coltonmorrill.com/" ImagePath="/vaporwave-01.svg" ImageAlt="Icon of the DOCS Logo" ProjectName="Vaporwave 3D Demo" ProjectSoftware="Threejs" />
@@ -123,11 +123,11 @@ export default function Home() {
       <div id='about-me' className='scroll-spacer'></div>
       <div className='flex justify-center items-center w-full mt-8 mb-10 lg:mb-40 flex-col lg:flex-row max-w-screen-lg mx-auto'>
         <div className='mx-10'>
-          <Image className='rounded-2xl' src="/colton-anna.jpeg" width={600} height={100} alt="Picture of Colton Morrill with his wife Anna, as they pose against a rocky wooded background" />
+          <Image className='rounded-2xl' src="/profile.jpg" width={600} height={100} alt="Picture of Colton Morrill posing for the camera." />
         </div>
         <div className='m-10'>
           <p className='font-bold mb-4'>Based in <RainbowText text="Salt Lake City, Utah" /></p>
-          <p className='font-bold mb-4'>I am married and have one cat and one guinea pig. In my free time I like to play piano, play video games, and make digital art.</p>
+          <p className='font-bold mb-4'>I am married and have one cat and two guinea pigs. In my free time I like to play piano, play video games, and make digital art.</p>
           <p className='font-bold mb-2'>I graduated from Brigham Young University - Idaho with a degree in <RainbowText text="Web Design and Developement with an emphasis on Design" />.</p>
           <p className='mb-4 text-sm text-gray-300'>While in school I focused heavily on the fundamentals of creating applications for the web alongside gaining exceptional skills in graphic design. </p>
         </div>
@@ -136,7 +136,7 @@ export default function Home() {
       <div className='mt-8 mb-20 flex flex-col items-center'>
         <h3 className='text-4xl font-bold text-center mb-20'>Experience</h3>
         <div>
-          <ExperienceCard ImagePath="/vibility-icon.png" ImageAlt="Icon of the Vibility Logo" JobPosition="Frontend Developer" Company="Vibility | Full Time" Duration="December 2022 - Present" />
+          <ExperienceCard ImagePath="/icon.png" ImageAlt="Icon of the Vibility Logo" JobPosition="Frontend Developer" Company="Vibility | Full Time" Duration="December 2022 - Present" />
           <ExperienceCard ImagePath="/tackicon.png" ImageAlt="Icon of the Tack Systems Logo" JobPosition="Lead Web Designer" Company="Tack Systems LLC | Full Time" Duration="April 2021 - November 2022 | 1 yr 7 mo" />
           <ExperienceCard ImagePath="/seo-rei.jpg" ImageAlt="CEO of SEO4REI" JobPosition="Director of Web/UI/UX Design" Company="SEO For Real Estate Investors | Full Time" Duration="November 2020 - April 2021 | 5 mo" />
           <ExperienceCard ImagePath="/seo-rei.jpg" ImageAlt="CEO of SEO4REI" JobPosition="Web Designer and SEO Support" Company="SEO For Real Estate Investors | Full Time" Duration="June 2020 - November 2021 | 5 mo" />
